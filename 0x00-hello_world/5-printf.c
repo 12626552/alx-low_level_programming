d674c694cf54cdecab8ec3cf1fdc96906bedca3b
12626552 <berrekket@gmail.com>
2022-12-08 05:10:31 -0800

    task five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stido.h>
+
 /**
- * main - Entry
+ * main - Entry Point
+ *
  * Return: Always 0
  */
+
 int main(viod)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
