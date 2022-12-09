1f1b082a2357662fdd7ece5a03fb589233bf8be0
12626552 <berrekket@gmail.com>
2022-12-08 23:18:21 -0800

    task five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stido.h>
 /**
- * main - Entry
- * Return: Always 0
- */
+ * main - print a c program using printf
+ *
+ * Return: 0
+*/
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
