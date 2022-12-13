4c371bd1f9c8165da8d9e7698c0828bd69f74899
12626552 <berrekket@gmail.com>
2022-12-13 08:12:57 -0800

    task five

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,24 @@
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
+
 		_putchar('+');
 		return (0);
 	}
 	else if (n == 0)
 	{
+
 		_putchar('0');
 		return (0);
 	}
 	else
 	{
+
 		_putchar('-');
 		return (-1);
+
 	}
 }
-
